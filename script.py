2c1ca2678e9ab67a5b1eaf55aae5c9fa5b82cfe6
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2017-09-11 17:48:06 +0200

    Speed Update
    * Optimized the script with Visual Studio Code - Insiders (variables, enumeration, imports, ...)
    * Removed re line at the end because of  the Update of the SendGmailApi (Characters like 'Ä' will be displayed again)

--- script.py
+++ script.py
@@ -6,24 +6,23 @@ This script scraps a table from a specific website, writes it in json format to 
 and sends an email to the recipients.
 """
 
-# My imports (the crawler)
-import json
-import re
-import logging
 import datetime
+import json
+import logging
 import os
+import re
 import requests
 from bs4 import BeautifulSoup
 
 # Gmail Imports (not important for the actual crawler)
 from SimplifiedGmailApiSubmodule.SendGmailSimplified import SimplifiedGmailApi
 
-
 # Paths for important directories and files - from home directory
 HOME_DIR = os.path.expanduser('~')
 
 # change this to the directory your script is: !!!!!!!!!!!!!!!!!
-DIR_OF_SCRIPT = os.path.join(HOME_DIR, os.path.join("Documents", "SubstitutePlanNotifierAEG"))
+DIR_OF_SCRIPT = os.path.join(
+    HOME_DIR, os.path.join("Documents", "SubstitutePlanNotifierAEG"))
 # "Documents"
 # "Documents/GitHubBeta"
 
@@ -37,14 +36,18 @@ PATH_FOR_HTML_FILE = os.path.join(DIRECTORY_FOR_DATA, 'html.json')
 
 
 # Setup the Gmail API - set USE_GMAIL False if you want to use the Simplified Gmail API
-USE_GMAIL = True
+USE_GMAIL = False
 if USE_GMAIL:
-    DIR_OF_GMAIL_API_FILES = os.path.join(DIR_OF_SCRIPT, os.path.join("SimplifiedGmailApiSubmodule", "gmail_api_files"))
-    PATH_OF_CLIENT_DATA = os.path.join(DIR_OF_GMAIL_API_FILES, "client_data.json")
-    PATH_OF_CLIENT_SECRET = os.path.join(DIR_OF_GMAIL_API_FILES, "client_secret.json")
-    GmailServer = SimplifiedGmailApi(PATH_OF_CLIENT_DATA, PATH_OF_CLIENT_SECRET, DIR_OF_GMAIL_API_FILES)
+    DIR_OF_GMAIL_API_FILES = os.path.join(
+        DIR_OF_SCRIPT, os.path.join("SimplifiedGmailApiSubmodule", "gmail_api_files"))
+    PATH_OF_CLIENT_DATA = os.path.join(
+        DIR_OF_GMAIL_API_FILES, "client_data.json")
+    PATH_OF_CLIENT_SECRET = os.path.join(
+        DIR_OF_GMAIL_API_FILES, "client_secret.json")
+    GMAIL_SERVER = SimplifiedGmailApi(
+        PATH_OF_CLIENT_DATA, PATH_OF_CLIENT_SECRET, DIR_OF_GMAIL_API_FILES)
 else:
-    GmailServer = None
+    GMAIL_SERVER = None
 
 
 # Check if the directory for the tables exists, if not create it
@@ -53,11 +56,11 @@ if not os.path.exists(DIRECTORY_FOR_TABLES):
 
 # load all websites and recipients
 with open(PATH_FOR_WEBSITES, "r") as websites_file:
-    websites = json.load(websites_file)
-    numberOfWebsites = len(websites)
+    WEBSITES = json.load(websites_file)
+    NUMBER_OF_ALL_WEBSITES = len(WEBSITES)
 # load json with all html information
 with open(PATH_FOR_HTML_FILE, "r") as html_file:
-    html_data = json.load(html_file)
+    HTML_DATA = json.load(html_file)
 
 # log file
 logging.basicConfig(filename=PATH_FOR_LOG, level=logging.DEBUG)
@@ -76,11 +79,11 @@ def extract_important_information(html_table):
     """
 
     # for every row
-    for a in range(len(html_table)):
+    for row, _ in enumerate(html_table):
         # remove the first column (class id)
-        del html_table[a][0]
-        for b in range(len(html_table[a])):
-            new_text = html_table[a][b]
+        del html_table[row][0]
+        for column, val in enumerate(html_table[row]):
+            new_text = val
             # remove newlines and more
             new_text = "".join(new_text.split("-\n"))
             new_text = "".join(new_text.split("\n"))
@@ -89,16 +92,16 @@ def extract_important_information(html_table):
             new_text = re.sub(r"^\s*(-\s*)?|(\s*-)?\s*$", "", new_text)
             # special thing (German: "H. to Hour")
             new_text = "Stunde".join(new_text.split("Std."))
-            html_table[a][b] = new_text
+            html_table[row][column] = new_text
 
-            if b > 2:
-                if not html_table[a][3]:
-                    html_table[a][b] = ""
+            if column > 2:
+                if not html_table[row][3]:
+                    html_table[row][column] = ""
 
     return html_table
 
 
-def create_html_message(json_table, class_url, email_date_string):
+def create_html_message(json_table, class_url, date_and_week):
     """Create the HTML email.
 
     Args:
@@ -109,44 +112,43 @@ def create_html_message(json_table, class_url, email_date_string):
         The complete Html email as a String.
     """
 
-    html_message = html_data["head"]
-    html_message += html_data["title-body"]
-    html_message += html_data["top"]
-    html_message += email_date_string
-    html_message += html_data["top2"]
+    html_message = HTML_DATA["head"]
+    html_message += HTML_DATA["title-body"]
+    html_message += HTML_DATA["top"]
+    html_message += date_and_week
+    html_message += HTML_DATA["top2"]
 
     # convert Json list to a custom and clean html table
-    for a in range(1, len(json_table)):
-        html_message += html_data["tr-start"]
-        if not json_table[a][3]:
-            for b in range(len(json_table[a])):
-                html_message += html_data["td-start"] + html_data["strike-start"]
-                if b < 3:
-                    html_message += json_table[a][b]
+    for row in range(1, len(json_table)):
+        html_message += HTML_DATA["tr-start"]
+        if not json_table[row][3]:
+            for column, val in enumerate(json_table[row]):
+                html_message += HTML_DATA["td-start"] + HTML_DATA["strike-start"]
+                if column < 3:
+                    html_message += val
                 else:
                     html_message += "---"
-                html_message += html_data["strike-end"] + html_data["td-end"]
+                html_message += HTML_DATA["strike-end"] + HTML_DATA["td-end"]
         else:
-            for b in range(len(json_table[a])):
-                html_message += html_data["td-start"]
-                html_message += json_table[a][b]
-                html_message += html_data["td-end"]
-        html_message += html_data["tr-end"]
-    html_message += html_data["middle"]
+            for column, val in enumerate(json_table[row]):
+                html_message += HTML_DATA["td-start"]
+                html_message += val
+                html_message += HTML_DATA["td-end"]
+        html_message += HTML_DATA["tr-end"]
+    html_message += HTML_DATA["middle"]
     html_message += class_url
-    html_message += html_data["space-between-link-and-text"]
-    html_message += html_data["linktext-plan"]
-    html_message += html_data["bottom"]
-    html_message += html_data["bottom-text-01"]
-    html_message += html_data["bottom-text-space-01"]
-    html_message += html_data["bottom-text-02"]
-    html_message += html_data["bottom-text-space-02"]
-    html_message += html_data["bottom-text-03"]
-    html_message += html_data["bottom-text-space-03"]
-    html_message += html_data["bottom-text-04"]
-    html_message += html_data["end"]
+    html_message += HTML_DATA["space-between-link-and-text"]
+    html_message += HTML_DATA["linktext-plan"]
+    html_message += HTML_DATA["bottom"]
+    html_message += HTML_DATA["bottom-text-01"]
+    html_message += HTML_DATA["bottom-text-space-01"]
+    html_message += HTML_DATA["bottom-text-02"]
+    html_message += HTML_DATA["bottom-text-space-02"]
+    html_message += HTML_DATA["bottom-text-03"]
+    html_message += HTML_DATA["bottom-text-space-03"]
+    html_message += HTML_DATA["bottom-text-04"]
+    html_message += HTML_DATA["end"]
 
-    # html_message.encode('us-ascii', 'ignore')
     html_message.encode('ascii', 'xmlcharrefreplace')
 
     return html_message
@@ -196,17 +198,19 @@ def compare_lists(old_list, new_list):
         # not "not is" but "!=" is used because it doesn't work after 256:
         # https://stackoverflow.com/questions/2239737/is-it-better-to-use-is-or-for-number-comparison-in-python
         print("len(old_list) " + str(len(old_list)) + " is not len(new_list) " + str(len(new_list)))
-        logging.info("Change because of: old list (" + str(len(old_list)) + ") and new list (" + str(len(new_list))
-                     + ") have different lengths")
+        logging.info("Change because of: old list (" + str(len(old_list)) + ") and new list ("
+                     + str(len(new_list)) + ") have different lengths")
         return True
 
     # if they are the same size iterate over them to find a difference
-    for a in range(1, len(new_list)):
-        for b in range(len(new_list[a])):
-            if new_list[a][b] is not old_list[a][b]:
-                print("new_list[a][b]:" + new_list[a][b] + " and old_list[a][b]: " + old_list[a][b])
-                logging.info("Change because of: at [" + str(a) + "][" + str(b) + "] old list is \"" +
-                             old_list[a][b] + "\" and new list is \"" + new_list[a][b] + "\"")
+    for row in range(1, len(new_list)):
+        for column, val in enumerate(new_list[row]):
+            if val is not old_list[row][column]:
+                print("new_list[a][b]:" + val + " and old_list[a][b]: "
+                      + old_list[row][column])
+                logging.info("Change because of: at [" + str(row) + "][" + str(column)
+                             + "] old list is \"" + old_list[row][column]
+                             + "\" and new list is \"" + val + "\"")
                 return True
 
     # or not - they must be the same
@@ -215,25 +219,27 @@ def compare_lists(old_list, new_list):
 
 
 # get time - https://stackoverflow.com/a/43302976/7827128
-date_time = datetime.datetime.now()
-date = date_time.date()  # gives date
-time = date_time.time()  # gives time
+DATE_TIME = datetime.datetime.now()
+DATE = DATE_TIME.date()  # gives date
+TIME = DATE_TIME.time()  # gives time
 
-# zfill adds leading zeros if number length is below parameter - https://stackoverflow.com/a/733478/7827128
-date_string = str(date.year) + "." + str(date.month).zfill(2) + "." + str(date.day).zfill(2)
-time_string = str(time.hour).zfill(2) + ":" + str(time.minute).zfill(2) + ":" + str(time.second).zfill(2)
+# zfill adds leading zeros if number length is below parameter
+# https://stackoverflow.com/a/733478/7827128
+DATE_STRING = str(DATE.year) + "." + str(DATE.month).zfill(2) + "." + str(DATE.day).zfill(2)
+TIME_STRING = str(TIME.hour).zfill(2) + ":" + str(TIME.minute).zfill(2) + ":" + str(
+    TIME.second).zfill(2)
 
-logging.info("script started on " + date_string + " at " + time_string)
+logging.info("script started on " + DATE_STRING + " at " + TIME_STRING)
 
 
 #    logging.info('So should this')
 #    logging.warning('And this, too')
 
 # check if there are any websites
-if numberOfWebsites > 0:
+if NUMBER_OF_ALL_WEBSITES > 0:
 
     # for every website get table
-    for x in websites:
+    for x in WEBSITES:
 
         print(">> Check for the class " + x["name"] + ":")
         logging.info("check the website " + x["url"] + " for the class " + x["name"])
@@ -247,7 +253,11 @@ if numberOfWebsites > 0:
             print(content)
             # extract the second table
             soup = BeautifulSoup(content, 'html.parser')
-            email_date_string = str(soup.find_all('b')[2].get_text()).replace("Vertretungsplan Klassen", "").lstrip(" ")
+
+            # extract the date and the week number
+            email_date_string = str(soup.find_all('b')[2].get_text())
+            # remove a String and all whitespaces at the begin and the end
+            email_date_string = email_date_string.replace("Vertretungsplan Klassen", "").lstrip(" ")
 
             #soup = BeautifulSoup(content, 'html.parser')
             table = soup.find_all('table')[1]
@@ -270,7 +280,8 @@ if numberOfWebsites > 0:
             # when the file doesn't exists set it to None
             except os.error:
                 old_json_table = None
-                logging.warning("The file " + DIRECTORY_FOR_TABLES + "/" + x["name"] + ".json was not found")
+                logging.warning("The file " + DIRECTORY_FOR_TABLES + "/" + x["name"]
+                                + ".json was not found")
 
         # this error comes up when there is no Website
         except IndexError:
@@ -278,22 +289,22 @@ if numberOfWebsites > 0:
             print("Website could not be crawled!")
             logging.warning("The website " + x["url"] + "could not be crawled")
         # More catches thanks to https://stackoverflow.com/a/16511493/7827128
-        except requests.exceptions.HTTPError as e:
+        except requests.exceptions.HTTPError as exception1:
             new_json_table, old_json_table = None, None
             # http error (414, etc.)
-            logging.warning(e)
-        except requests.exceptions.Timeout as e:
+            logging.warning(exception1)
+        except requests.exceptions.Timeout as exception2:
             new_json_table, old_json_table = None, None
             # Maybe set up for a retry, or continue in a retry loop
-            logging.warning(e)
-        except requests.exceptions.TooManyRedirects as e:
+            logging.warning(exception2)
+        except requests.exceptions.TooManyRedirects as exception3:
             new_json_table, old_json_table = None, None
             # Tell the user their URL was bad and try a different one
-            logging.warning(e)
-        except requests.exceptions.RequestException as e:
+            logging.warning(exception3)
+        except requests.exceptions.RequestException as exception4:
             new_json_table, old_json_table = None, None
             # catastrophic error. bail.
-            logging.warning(e)
+            logging.warning(exception4)
 
         # if there is no old file, a new one, that is different to the old one continue
         if compare_lists(old_json_table, new_json_table):
@@ -305,20 +316,18 @@ if numberOfWebsites > 0:
                               ".json was successfully overwritten with the new list")
 
             # 2. send it to all recipients an email with the change
-            if USE_GMAIL and GmailServer is not None:
+            if USE_GMAIL and GMAIL_SERVER is not None:
 
                 subject = "Neuer Vertretungsplan Klasse " + x["name"]
 
                 message_text = create_html_message(table_data, x["url"], email_date_string)
-
-                message_text = re.sub(r'[^\x00-\x7F]+', ' ', message_text)
 
                 print("Message:")
                 print(message_text.encode('ascii', 'xmlcharrefreplace').decode('ascii'))
                 print("send to")
 
                 for y in x["recipients"]:
-                    if GmailServer.send_html(y, subject, message_text):
+                    if GMAIL_SERVER.send_html(y, subject, message_text):
                         logging.info(">> Email was successfully sent to " + y)
                     else:
                         logging.debug(">> Email was not sent to " + y)
